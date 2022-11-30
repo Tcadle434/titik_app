1a6fd9da1c277d9db66eb2bcdb9df94992dadefc
Tcadle434 <thomascadle434@gmail.com>
2022-11-29 18:22:44 -0700

    NextJs + tailwind + sanity full stack tik tik app iteration one

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      'bafybeibl2zhzhbdbhi4k2pzi4hln6e27lgs5js7puf4tfymczhomqrk7cu.ipfs.nftstorage.link',
+      'lh3.googleusercontent.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,36 @@
-import { NextPage } from 'next';
+import axios from 'axios';
+import NoResults from '../components/NoResults';
+import VideoCard from '../components/VideoCard';
 
-const Home: NextPage = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+import { Video } from '../types';
+
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
+  return (
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={'No videos'} />
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  // console.log(response.data.name);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
 };
 
 export default Home;
