a468819f21a21057be97ffe00481fc6c43047155
Tcadle434 <thomascadle434@gmail.com>
2022-12-01 01:03:22 -0700

    user profiles and details and suggested accounts

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BiCommentX } from 'react-icons/bi';
+import { MdOutlineVideocamOff } from 'react-icons/md';
 
 interface IProps {
   text: string;
 }
 
 const NoResults = ({ text }: IProps) => {
-  return <div>No Results</div>;
+  return (
+    <div className='flex flex-col justify-center items-center h-full w-full'>
+      <p className='text-8xl'>
+        <MdOutlineVideocamOff />
+      </p>
+      <p className='text-2xl text-center'>{text}</p>
+    </div>
+  );
 };
 
 export default NoResults;
